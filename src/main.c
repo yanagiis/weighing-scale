a74f6e53e68136ae05014fea0fc061a529cbd678
jimmy.ou <yanagiis@gmail.com>
2022-06-18 21:48:20 +0800

    feat: add stm32_min_dev_black overlay

--- src/main.c
+++ src/main.c
@@ -1,112 +1,80 @@
+#include "sensor/hx711/hx711.h"
+
 #include <stdio.h>
 #include <string.h>
 
 #include <device.h>
-#include <drivers/display.h>
 #include <drivers/gpio.h>
-#include <lvgl.h>
+#include <drivers/sensor.h>
+#include <dt-bindings/dt-util.h>
 #include <zephyr.h>
 
 #define LOG_LEVEL CONFIG_LOG_DEFAULT_LEVEL
 #include <logging/log.h>
 LOG_MODULE_REGISTER(app);
 
-#define HX711_POWER_PIN 27
-#define HX711_RX_PIN	13
-#define HX711_TX_PIN	12
+struct moving_average {
+	double window[16];
+	size_t victim;
+	size_t len;
+};
 
-static struct k_work hx711_work;
-static struct gpio_callback gpio_cb;
-
-static void hx711_ready_cb(const struct device *port, struct gpio_callback *cb,
-			   gpio_port_pins_t pins);
-static void hx711_recv(struct k_work *work);
-
-void main(void)
+static void moving_average_init(struct moving_average *ma)
 {
-	k_work_init(&hx711_work, hx711_recv);
-
-	const struct device *gpio0 = DEVICE_DT_GET(DT_ALIAS(gpio0));
-	gpio_pin_configure(gpio0, HX711_POWER_PIN,
-			   GPIO_OUTPUT | GPIO_OUTPUT_INIT_HIGH);
-	gpio_pin_configure(gpio0, HX711_RX_PIN, GPIO_INPUT);
-	gpio_pin_configure(gpio0, HX711_TX_PIN,
-			   GPIO_OUTPUT | GPIO_OUTPUT_INIT_LOW);
-
-	gpio_init_callback(&gpio_cb, hx711_ready_cb, BIT(HX711_RX_PIN));
-	gpio_add_callback(gpio0, &gpio_cb);
-
-	gpio_pin_interrupt_configure(gpio0, HX711_RX_PIN,
-				     GPIO_INT_EDGE_FALLING);
+	ma->victim = 0;
+	ma->len = 0;
 }
 
-static void display(void)
+static void moving_average_put(struct moving_average *ma, double value)
 {
-	const struct device *display_dev = device_get_binding("ST7789V");
-	if (!device_is_ready(display_dev)) {
-		LOG_ERR("Device not ready, aborting test");
-		return;
+	ma->window[ma->victim] = value;
+	ma->victim = (ma->victim + 1) % ARRAY_SIZE(ma->window);
+	if (ma->len != ARRAY_SIZE(ma->window)) {
+		ma->len++;
 	}
+}
 
-	const struct device *gpio0 = DEVICE_DT_GET(DT_ALIAS(gpio0));
-	gpio_pin_configure(gpio0, 4, GPIO_OUTPUT | GPIO_OUTPUT_INIT_HIGH);
-
-	lv_obj_t *hello_world_label;
-	if (IS_ENABLED(CONFIG_LV_Z_POINTER_KSCAN)) {
-		lv_obj_t *hello_world_button;
-
-		hello_world_button = lv_btn_create(lv_scr_act());
-		lv_obj_align(hello_world_button, LV_ALIGN_CENTER, 0, 0);
-		hello_world_label = lv_label_create(hello_world_button);
-	} else {
-		hello_world_label = lv_label_create(lv_scr_act());
+static double moving_average_get(struct moving_average *ma)
+{
+	double avg = 0;
+	for (size_t i = 0; i < ma->len; i++) {
+		avg += ma->window[i];
 	}
+	avg /= ma->len;
+	return avg;
+}
 
-	lv_label_set_text(hello_world_label, "Hello world!");
-	lv_obj_align(hello_world_label, LV_ALIGN_CENTER, 0, 0);
+void hx711_monitor(void)
+{
+	const struct device *hx711 = DEVICE_DT_GET(DT_NODELABEL(hx711));
+	__ASSERT(hx711 == NULL, "Failed to get device binding");
 
-	lv_obj_t *count_label = lv_label_create(lv_scr_act());
-	lv_obj_align(count_label, LV_ALIGN_BOTTOM_MID, 0, 0);
+	struct moving_average ma;
+	moving_average_init(&ma);
 
-	lv_task_handler();
-	display_blanking_off(display_dev);
+	k_msleep(100);
+	avia_hx711_tare(hx711, 100);
 
-	LOG_INF("ready");
-	uint32_t count = 0U;
-	char count_str[11] = {0};
-	while (1) {
-		if ((count % 100) == 0U) {
-			sprintf(count_str, "%d", count / 100U);
-			lv_label_set_text(count_label, count_str);
+	while (true) {
+		k_msleep(50);
+
+		int ret = sensor_sample_fetch(hx711);
+		if (ret != 0) {
+			LOG_ERR("Cannot take measurement: %d", ret);
+			continue;
 		}
-		lv_task_handler();
-		k_sleep(K_MSEC(10));
-		++count;
+
+		struct sensor_value v;
+		sensor_channel_get(hx711, (int)HX711_SENSOR_CHAN_WEIGHT, &v);
+
+		double weight = sensor_value_to_double(&v);
+		if (weight >= -50 && weight <= 4000) {
+			moving_average_put(&ma, weight);
+		}
+
+		double average = moving_average_get(&ma);
+		printf("avg %f weight %f\n", average, weight);
 	}
 }
 
-static void hx711_ready_cb(const struct device *port, struct gpio_callback *cb,
-			   gpio_port_pins_t pins)
-{
-	k_work_submit(&hx711_work);
-}
-
-static void hx711_recv(struct k_work *work)
-{
-	const struct device *gpio0 = DEVICE_DT_GET(DT_ALIAS(gpio0));
-
-	uint32_t raw = 0;
-	for (int i = 0; i < 25; i++) {
-		raw <<= 1;
-
-		gpio_port_set_bits_raw(gpio0, BIT(HX711_TX_PIN));
-
-		gpio_port_value_t v;
-		gpio_port_get(gpio0, &v);
-
-		raw |= (v & BIT(HX711_RX_PIN)) >> (HX711_RX_PIN);
-		gpio_port_clear_bits_raw(gpio0, BIT(HX711_TX_PIN));
-	}
-}
-
-K_THREAD_DEFINE(display_id, 1024, display, NULL, NULL, NULL, 7, 0, 0);
+K_THREAD_DEFINE(hx711_id, 1024, hx711_monitor, NULL, NULL, NULL, -1, 0, 0);
