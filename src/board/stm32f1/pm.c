7136d80f41285dd7da362744a1f4ce4547b65c39
jimmy.ou <yanagiis@gmail.com>
2022-06-30 14:39:12 +0800

    feat: board: stm32f1: enable periph pwr for waking up from standby mode

--- src/board/stm32f1/pm.c
+++ src/board/stm32f1/pm.c
@@ -1,10 +1,11 @@
+#include <zephyr/device.h>
+#include <zephyr/pm/pm.h>
+#include <zephyr/zephyr.h>
+
 #include <stm32f1xx_ll_bus.h>
 #include <stm32f1xx_ll_cortex.h>
 #include <stm32f1xx_ll_pwr.h>
 #include <stm32f1xx_ll_rcc.h>
-
-#include <zephyr/zephyr.h>
-#include <zephyr/pm/pm.h>
 
 void pm_state_set(enum pm_state state, uint8_t substate_id)
 {
@@ -12,6 +13,7 @@ void pm_state_set(enum pm_state state, uint8_t substate_id)
 
 	switch (state) {
 	case PM_STATE_SOFT_OFF:
+		LL_PWR_DisableBkUpAccess();
 		LL_PWR_EnableWakeUpPin(LL_PWR_WAKEUP_PIN1);
 		LL_PWR_ClearFlag_WU();
 		LL_PWR_SetPowerMode(LL_PWR_MODE_STANDBY);
@@ -23,3 +25,21 @@ void pm_state_set(enum pm_state state, uint8_t substate_id)
 		break;
 	}
 }
+
+/* Initialize STM32 Power */
+static int stm32_pm_init(const struct device *dev)
+{
+	ARG_UNUSED(dev);
+
+	/* enable Power clock */
+	LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_PWR);
+
+#ifdef CONFIG_DEBUG
+	/* Enable the Debug Module during STOP mode */
+	LL_DBGMCU_EnableDBGStopMode();
+#endif /* CONFIG_DEBUG */
+
+	return 0;
+}
+
+SYS_INIT(stm32_pm_init, PRE_KERNEL_1, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT);
